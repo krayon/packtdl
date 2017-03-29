1e25bbcd8e9cdd6667fd3614abb2de311205ce87
Krayon <krayon.git@qdnx.org>
2017-03-30 02:19:36 +1100

    Added GPL v2 license

--- packtfreedl.bash
+++ packtfreedl.bash
@@ -1,5 +1,26 @@
 #!/bin/bash
 # vim:set tabstop=4 textwidth=80 shiftwidth=4 expandtab cindent cino=(0,ml,\:0:
+# ( settings from: http://datapax.com.au/code_conventions/ )
+#
+#/**********************************************************************
+#    Packt Free DL
+#    Copyright (C) 2017 Todd Harbour
+#
+#    This program is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License
+#    version 2 ONLY, as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program, in the file COPYING or COPYING.txt; if
+#    not, see http://www.gnu.org/licenses/ , or write to:
+#      The Free Software Foundation, Inc.,
+#      51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# **********************************************************************/
 
 # packtfreedl
 #------------
